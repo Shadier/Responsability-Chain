86ff5dd2ff603a1a34d6dde4d2ae13b5611b249e
Raúl Alejandro Moreno Santos <ramoreno@scio.local>
2019-09-05 14:36:21 -0500

    Tarea terminada

---
+++ ResponsabilityChain/BL/Secretary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsabilityChain.BL
+{
+    public class Secretary : Approver
+    {
+        public override void ProcessRequest(Purchase purchase)
+        {
+            if (purchase.Amount < 50000.0)
+            {
+                Console.WriteLine("{0} approved request# {1} ('{2}')",
+                  this.GetType().Name, purchase.Number, purchase.Purpose);
+            }
+            else if (successor != null)
+            {
+                successor.ProcessRequest(purchase);
+            }
+        }
+    }
+}
